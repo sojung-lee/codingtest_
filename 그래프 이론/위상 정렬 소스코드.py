71d6911a9d2a9f4daab5e9b989c1723645193d4a
sojung-lee <skyhigh01237@gmail.com>
2022-04-02 21:51:48 +0900

    coding

---
+++ 그래프 이론/위상 정렬 소스코드.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+v, e = map(int, input().split())
+# 모든 노드에 대한 진입차수는 0으로 초기화
+indegree = [0] * (v + 1)
+
+# 각 노드에 연결된 간선 정보를 담기 위한 연결 리스트(그래프) 초기화
+graph = [[] for i in range(v + 1)]
+
+# 방향 그래프의 모든 간선 정보를 입력받기
+for _ in range(e):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    #진입차수를 1증가
+    indegree[b] += 1
+
+# 위상 정렬 함수
+def topology_sort():
+    result = [] #알고리즘 수행 결과를 담을 리스트
+    q = deque()
+
+    # 처음 시작할 때는 진입차수가 0인 노드를 큐에 삽입
+    for i in range(1, v + 1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    # 큐가 빌 때까지 반복
+    while q:
+        now = q.popleft()
+        result.append(now)
+
+        for i in graph[now]:
+            indegree[i] -= 1
+            # 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
+            if indegree[i] == 0:
+                q.append(i)
+    for i in result:
+        print(i, end=' ')
+
+topology_sort()
---
+++ 최단 경로/전보.py
@@ -0,0 +1,41 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m, start = map(int, input().split())
+graph = [[]for i in range(n + 1)]
+distance = [INF] * (n+1)
+
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0,start))
+    distance[start] = 0
+    while q:
+        # 가장 최단 거리가 짧은 노드에 대한 정보를 꺼내기
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        #현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+            #현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+count = 0
+max_distance = 0
+for d in distance:
+    if d != INF:
+        count += 1
+        max_distance = max(max_distance, d)
+
+print(count - 1, max_distance)
