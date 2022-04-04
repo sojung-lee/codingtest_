597067612fdc0677c500ab107181e3457ab90711
sojung-lee <skyhigh01237@gmail.com>
2022-04-04 16:56:57 +0900

    coding

---
+++ 그리디문제/Q06. 무지의 먹방 라이브.py
@@ -0,0 +1,27 @@
+#https://programmers.co.kr/learn/courses/30/lessons/42891?language=python3
+
+import heapq
+
+def solution(food_times, k):
+    if sum(food_times) <= k:
+        return -1
+
+    q = []
+    for i in range(len(food_times)):
+        # (음식 시간, 음식 번호) 형태로 우선순위 큐에 삽입
+        heapq.heappush(q, (food_times[i], i + 1))
+
+    sum_value = 0 # 먹기 위해 사용한 시간
+    previous = 0 # 직전에 다 먹은 음식 시간
+
+    length = len(food_times) # 남은 음식의 개수
+
+    # sum_value + (현재의 음식 시간 - 이전 음식 시간) * 현재 음식 개수와 k 비교
+    while sum_value + ((q[0][0] - previous) * length) <= k:
+        now = heapq.heappush(q)[0]
+        sum_value += (now - previous) * length
+        length -= 1
+        previous = now
+
+    result = sorted(q, key = lambda x: x[1])
+    return result[(k - sum_value) % length][1]
